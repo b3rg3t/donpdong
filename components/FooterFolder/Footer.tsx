24345ba91aca87a58f26f67f3fd9fbdad9e10925
b3rg3t <berget_90db@hotmail.com>
2020-01-29 16:25:14 +0100

    footer styling

--- components/FooterFolder/Footer.tsx
+++ components/FooterFolder/Footer.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import "./footer.scss";
 const Footer = (): React.ReactElement => {
-  return <><footer>Hej</footer></>;
+  return (
+    <>
+      <footer><p>Hej</p></footer>
+    </>
+  );
 };
 
 export default Footer;
