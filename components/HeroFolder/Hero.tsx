ec701288d2b9a0f3a5e775af724c77143f4a3aee
b3rg3t <berget_90db@hotmail.com>
2020-01-30 09:12:30 +0100

    hero with overlay done

--- components/HeroFolder/Hero.tsx
+++ components/HeroFolder/Hero.tsx
@@ -1,12 +1,28 @@
 import React from "react";
 import heights from "../../helpers/picturehelp";
+import "./hero.scss";
 
 const FirstComponent = (): React.ReactElement => {
-  const heroHeight = `100vh - ${heights.navHeigt}`;
   return (
     <>
-      <div className="hero" style={{ height: `${heroHeight}` }}>
-        yoyoyo
+      <div
+        className="hero"
+        style={{
+          height: `calc(100vh - ${heights.navHeigt}`,
+          backgroundImage: `url(./images/yogahero.jpg)`,
+          backgroundSize: "cover",
+          backgroundPosition: "center center"
+        }}
+      >
+        <div className="hero__layer"></div>
+        <h1>Hello World</h1>
+        <p>
+          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eaque
+          labore nulla doloremque laborum, odit nostrum necessitatibus.
+          Assumenda eaque aperiam repellat nisi iste dolorum sint delectus
+          asperiores. A, harum consequuntur?
+        </p>
       </div>
     </>
   );
